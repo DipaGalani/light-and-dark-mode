e44038696c51126894c13f18359d4c8d35f40f95
Dipa Galani <dg180901@gmail.com>
2024-03-23 12:38:19 +0700

    finalized functionality - march 23

---
+++ script.js
@@ -0,0 +1,60 @@
+const toggleSwitch = document.querySelector("input[type=checkbox]");
+const nav = document.querySelector("#nav");
+const toggleIcon = document.querySelector("#toggle-icon");
+const image1 = document.querySelector("#image1");
+const image2 = document.querySelector("#image2");
+const image3 = document.querySelector("#image3");
+const textBox = document.querySelector("#text-box");
+
+// imageThemeControl function
+function imageThemeControl(color) {
+  image1.src = `./img/undraw_proud_coder_${color}.svg`;
+  image2.src = `./img/undraw_conceptual_idea_${color}.svg`;
+  image3.src = `./img/undraw_feeling_proud_${color}.svg`;
+}
+
+// darkMode function
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageThemeControl("dark");
+}
+
+// lightMode function
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageThemeControl("light");
+}
+
+// toggleSwitch function to toggle light and dark mode on the highest element on the DOM Tree
+function toggleTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    lightMode();
+  }
+}
+
+// Event Listener for Checkbox Element
+toggleSwitch.addEventListener("change", toggleTheme);
+
+// Check for 'theme' key in Local Storage
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
